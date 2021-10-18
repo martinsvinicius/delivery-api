00abeb112cd7d50ed1217d5aadd2c64632c507d9
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-10-18 12:22:12 -0300

    refactoring KitchenController and adding new methods

--- src/main/java/tech/blobteam/algafood/api/controller/KitchenController.java
+++ src/main/java/tech/blobteam/algafood/api/controller/KitchenController.java
@@ -1,9 +1,10 @@
 package tech.blobteam.algafood.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import tech.blobteam.algafood.model.Kitchen;
 import tech.blobteam.algafood.repository.kitchen.KitchenRepository;
 
@@ -12,21 +13,57 @@ import java.util.List;
 /** @author vinicius-victor */
 @RestController
 @RequestMapping("/kitchens")
+@RequiredArgsConstructor
 public class KitchenController {
 
   private final KitchenRepository repository;
 
-  public KitchenController(KitchenRepository kitchenRepository) {
-    repository = kitchenRepository;
-  }
-
   @GetMapping
-  public List<Kitchen> list() {
-    return repository.listAll();
+  public ResponseEntity<List<Kitchen>> list() {
+    return ResponseEntity.ok(repository.listAll());
   }
 
-  @GetMapping("/{id}")
-  public Kitchen find(@PathVariable Long id) {
-    return repository.findById(id);
+  @GetMapping("{id}")
+  public ResponseEntity<Kitchen> findById(@PathVariable Long id) {
+    final Kitchen kitchen = repository.findById(id);
+
+    if (kitchen == null) return ResponseEntity.notFound().build();
+
+    return ResponseEntity.ok(kitchen);
+  }
+
+  @PostMapping
+  public ResponseEntity<Kitchen> create(@RequestBody Kitchen kitchen) {
+    final Kitchen savedKitchen = repository.save(kitchen);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedKitchen);
+  }
+
+  @PutMapping("{id}")
+  public ResponseEntity<Kitchen> update(@PathVariable Long id, @RequestBody Kitchen kitchen) {
+    final Kitchen currentKitchen = repository.findById(id);
+
+    if (currentKitchen == null) return ResponseEntity.notFound().build();
+
+    currentKitchen.setName(kitchen.getName());
+
+    final Kitchen savedKitchen = repository.save(currentKitchen);
+
+    return ResponseEntity.ok(savedKitchen);
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<Kitchen> delete(@PathVariable Long id) {
+    final Kitchen kitchenExists = repository.findById(id);
+
+    if (kitchenExists == null) return ResponseEntity.notFound().build();
+
+    try {
+      repository.remove(kitchenExists);
+    } catch (DataIntegrityViolationException e) {
+      return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    return ResponseEntity.noContent().build();
   }
 }
