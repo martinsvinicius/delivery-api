cc26c3b18587752fc37bd789a7e8cba094f89c3b
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-10-18 12:23:41 -0300

    refactor: State entity

--- src/main/java/tech/blobteam/algafood/model/State.java
+++ src/main/java/tech/blobteam/algafood/model/State.java
@@ -3,16 +3,18 @@ package tech.blobteam.algafood.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /** @author vinicius */
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class State {
-  @Id private Long id;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @EqualsAndHashCode.Include
+  private Long id;
 
   @Column(nullable = false)
   private String name;
