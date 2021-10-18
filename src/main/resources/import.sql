9b44bde2daadd1c0d18b7f02cbc7f34e54ace1ea
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-10-18 12:21:09 -0300

    adding states to import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,12 @@
+-- kitchens
 insert into kitchen (name) values ('Tailandesa');
 insert into kitchen (name) values ('Indiana');
 
+-- restaurants
 insert into restaurant (delivery_price_rate, name, kitchen_id) values ( 10, 'Thai Gourmet', 1);
 insert into restaurant (delivery_price_rate, name, kitchen_id) values (9.50, 'Thai Delivery', 1);
 insert into restaurant (delivery_price_rate, name, kitchen_id) values (15, 'Tuk Tuk Indian Food', 2);
+
+-- states
+insert into public.state (name) values ('RIO GRANDE DO NORTE');
+insert into public.state (name) values ('SÃO PAULO');
