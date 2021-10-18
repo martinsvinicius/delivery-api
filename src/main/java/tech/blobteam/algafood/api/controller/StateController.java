8bb1d097ecc82482990c81da19d3033456644bfb
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-10-18 12:22:01 -0300

    refactoring StateController and adding new methods

--- src/main/java/tech/blobteam/algafood/api/controller/StateController.java
+++ src/main/java/tech/blobteam/algafood/api/controller/StateController.java
@@ -1,9 +1,9 @@
 package tech.blobteam.algafood.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import tech.blobteam.algafood.model.State;
 import tech.blobteam.algafood.repository.state.StateRepository;
 
@@ -12,20 +12,53 @@ import java.util.List;
 /** @author vinicius-victor */
 @RestController
 @RequestMapping("/states")
+@RequiredArgsConstructor
 public class StateController {
+
   private final StateRepository repository;
 
-  public StateController(StateRepository stateRepository) {
-    repository = stateRepository;
-  }
-
   @GetMapping
-  public List<State> list() {
-    return repository.listAll();
+  public ResponseEntity<List<State>> list() {
+    return ResponseEntity.ok(repository.listAll());
   }
 
-  @GetMapping("/{id}")
-  public State find(@PathVariable Long id) {
-    return repository.findById(id);
+  @GetMapping("{id}")
+  public ResponseEntity<State> findById(@PathVariable Long id) {
+    State state = repository.findById(id);
+
+    if (state == null) return ResponseEntity.notFound().build();
+
+    return ResponseEntity.ok(state);
+  }
+
+  @PostMapping
+  public ResponseEntity<State> create(@RequestBody State state) {
+    State savedState = repository.save(state);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedState);
+  }
+
+  @PutMapping("{id}")
+  public ResponseEntity<State> update(@PathVariable Long id, @RequestBody State state) {
+    final State currentState = repository.findById(id);
+
+    if (currentState == null) return ResponseEntity.notFound().build();
+
+    currentState.setName(state.getName());
+
+    final State savedState = repository.save(currentState);
+
+    return ResponseEntity.ok(savedState);
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<State> delete(@PathVariable Long id) {
+    State stateExists = repository.findById(id);
+
+    if (stateExists == null) return ResponseEntity.notFound().build();
+
+    repository.remove(stateExists);
+
+    return ResponseEntity.noContent().build();
   }
 }
