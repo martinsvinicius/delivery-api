72c575d012d3aa80f03781933cb5959926d463bb
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-10-25 11:30:16 -0300

    refactor RestaurantRegisterService save method

--- src/main/java/tech/blobteam/algafood/service/restaurant/RestaurantRegisterService.java
+++ src/main/java/tech/blobteam/algafood/service/restaurant/RestaurantRegisterService.java
@@ -5,7 +5,9 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import tech.blobteam.algafood.exception.EntityInUseException;
+import tech.blobteam.algafood.model.Kitchen;
 import tech.blobteam.algafood.model.Restaurant;
+import tech.blobteam.algafood.repository.kitchen.KitchenRepository;
 import tech.blobteam.algafood.repository.restaurant.RestaurantRepository;
 
 import javax.persistence.EntityNotFoundException;
@@ -15,9 +17,15 @@ import javax.persistence.EntityNotFoundException;
 @RequiredArgsConstructor
 public class RestaurantRegisterService {
 
-  final RestaurantRepository repository;
+  private final RestaurantRepository repository;
+  private final KitchenRepository kitchenRepository;
 
   public Restaurant save(Restaurant restaurant) {
+    Long kitchenId = restaurant.getKitchen().getId();
+    Kitchen kitchenExists = kitchenRepository.findById(kitchenId);
+
+    if (kitchenExists == null) throw new EntityNotFoundException("Kitchen does not exists");
+
     return repository.save(restaurant);
   }
 
